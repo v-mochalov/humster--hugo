06a56aabae4aae5076d93be32cf02c569a719e5f
v-mochalov <deathguuun@gmail.com>
2024-02-13 09:41:38 +0200

    test

--- functions/sendToTelegram.js
+++ functions/sendToTelegram.js
@@ -2,44 +2,58 @@ const https = require('https');
 const querystring = require('querystring');
 
 exports.handler = async (event, context) => {
-  const params = querystring.parse(event.body);
-  
-  const name = params['user_name'];
-  const phone = params['user_phone'];
-  const proj = params['user_project'];
-  const info = params['user_info'];
-  const token = '5346944790:AAGRQ1lDwryc9z4cX8LTjGjdFmQBTaxjPwY';
-  const chatId = '-623964129';
-  
-  const message = `
-    <b>E-mail:</b> ${name}%0A
-    <b>Телефон:</b> ${phone}%0A
-    <b>Проект:</b> ${proj}%0A
-    <b>Доп. информация:</b> ${info}%0A
-  `;
-  
-  const options = {
-    hostname: 'api.telegram.org',
-    port: 443,
-    path: `/bot${token}/sendMessage?chat_id=${chatId}&parse_mode=html&text=${message}`,
-    method: 'GET'
-  };
-  
-  const req = https.request(options, (res) => {
-    res.setEncoding('utf8');
-    res.on('data', (chunk) => {
-      console.log(`Response: ${chunk}`);
+  try {
+    // Получаем данные из тела запроса
+    const params = querystring.parse(event.body);
+    const name = params['user_name'];
+    const phone = params['user_phone'];
+    const proj = params['user_project'];
+    const info = params['user_info'];
+    const token = '5346944790:AAGRQ1lDwryc9z4cX8LTjGjdFmQBTaxjPwY';
+    const chatId = '-623964129';
+
+    // Формируем сообщение
+    const message = `
+      <b>E-mail:</b> ${name}%0A
+      <b>Телефон:</b> ${phone}%0A
+      <b>Проект:</b> ${proj}%0A
+      <b>Доп. информация:</b> ${info}%0A
+    `;
+
+    // Экранируем значение переменной message
+    const encodedMessage = encodeURIComponent(message);
+
+    // Настройки для запроса к Telegram API
+    const options = {
+      hostname: 'api.telegram.org',
+      port: 443,
+      path: `/bot${token}/sendMessage?chat_id=${chatId}&parse_mode=html&text=${encodedMessage}`,
+      method: 'GET'
+    };
+
+    // Отправляем запрос к Telegram API
+    const req = https.request(options, (res) => {
+      console.log(`statusCode: ${res.statusCode}`);
+
+      res.on('data', (d) => {
+        process.stdout.write(d);
+      });
     });
-  });
-  
-  req.on('error', (e) => {
-    console.error(`Problem with request: ${e.message}`);
-  });
-  
-  req.end();
-  
-  return {
-    statusCode: 200,
-    body: 'Message sent to Telegram successfully'
-  };
+
+    req.on('error', (error) => {
+      console.error(error);
+    });
+
+    req.end();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Сообщение успешно отправлено' })
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Произошла ошибка при отправке сообщения' })
+    };
+  }
 };
